5d2be79b767f7827bc446d6c2ebb3d7d195096a9
Pratham Dhawan <prathamdhawan@Prathams-MacBook-Pro.local>
2026-04-25 13:47:56 +0100

    Added testscript.py

---
+++ testscript.py
@@ -0,0 +1,12 @@
+# a simple program that returns sci names for a set of organism names
+
+user_input = input("enter a common name:")
+if user_input == "giant kelp":
+	print("Macrocystis pyrifera")
+if user_input == "bogue":
+        print("Boops boops")
+if user_input == "coelacanth":
+        print("class: Actinistia")
+if user_input == "siamese flat-barbelled catfish":
+        print("Platytropis siamensis")
+
